901da493ad12328609d89c599c0b0648a705f5ad
Markus Cozowicz <marcozo@microsoft.com>
2019-11-15 19:01:49 +0100

    remove initial .net proxy service

--- service/Personalizer-CMS-API/Personalizer/ContextFeatures.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.Personalizer.CMS.Personalizer
-{
-    public class ContextFeatures
-    {
-        public string CountryISO { get; set; }
-
-        public string StateISO { get; set; }
-    }
-}
